d082dbade2f6d83f926052b596ea95afb092aafb
Camilo Pino <camilo.pino12@inacapmail.cl>
2024-05-09 19:01:38 -0400

    Inicializacion de repositorio

--- src/index.js
+++ src/index.js
@@ -1,15 +1,60 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+//import { Ai } from '@cloudflare/ai'
+import { Hono } from 'hono/quick'
 
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+import blocking from './blocking.html'
+import streaming from './streaming.html'
+
+const app = new Hono()
+
+app.get('/', c => {
+	return c.html(streaming)
+})
+
+app.get('/b', c =>{
+	return c.html(blocking)
+})
+
+app.get('/stream', async c =>{
+	const ai = c.env.AI
+
+	const query = c.req.query("query") // ?query=Hola Como estas?
+	const question = query || 'How are you?'
+
+	const messages = [
+		{ role: "system", content: "Eres una amable asistente" },
+		{ role: "user", content: question },
+	]
+
+	const aiResponse = await ai.run(
+		'@cf/meta/llama-3-8b-instruct',
+		{ messages, stream: true }
+	)
+
+	return new Response(aiResponse, {
+		headers:{
+			'Content-Type': 'text/event-stream'
+		}
+	})
+})
+
+app.post('/', async c =>{
+	//const ai = new Ai(c.env.AI)
+	const ai = c.env.AI
+
+	const body = await c.req.json()
+	const question = body.query || 'How are you?'
+
+	const messages = [
+		{ role: "system", content: "Eres una amable asistente" },
+		{ role: "user", content: question },
+	]
+	
+	const aiResponse = await ai.run(
+		'@cf/meta/llama-3-8b-instruct',
+		{ messages }
+	)
+
+	return c.text(aiResponse.response)
+})
+
+export default app
